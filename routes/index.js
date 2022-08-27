4915283bad4b4e94e16594c9be4dba9c6830354f
Yati-Chourgade <chourgadeyati@gmail.com>
2022-08-28 00:16:00 +0530

    commit

--- routes/index.js
+++ routes/index.js
@@ -2,20 +2,7 @@ const express = require('express');
 const router = express.Router();
 const Product = require('../models/product');
 var Cart=require('../models/cart');
-//First Commit
-/* GET home page. */
-//  router.get('/', function(req, res, next) {
-//    Product.find(function(err,docs){
-//     var productChunk =[];
-//     var chunkSize = 3;
-//     for(var i=0 ; i<docs.length; i+=chunkSize){
-//         productChunk.push(docs.slice(i , i+chunkSize));
-//     }
-//    res.render('shop/index', { title: 'Shopping Cart' , products: productChunk});
-  
-//    });
-  
-//  });
+const {isLoggedIn}=require("../middleware")
 
 router.get('/add-to-cart/:id',function(req,res,next){
   const productId = req.params.id;
@@ -29,9 +16,10 @@ router.get('/add-to-cart/:id',function(req,res,next){
     console.log(req.session.cart);
     res.redirect('/');
   })
+
 })
 
-router.get('/', async (req, res, next) => {
+router.get('/',isLoggedIn, async (req, res, next) => {
     const products = await Product.find({});
     res.render('layouts/main-layout', { title: 'Shopping Cart', products });
   });
